3cde4e0c07e9e3431869c408817f0a0ca2101dba
samineni vinay <saminenivinay999@gmail.com>
2025-02-13 16:38:15 +0530

    Add routing for videos, tweets, subscriptions, playlists, likes, healthcheck, dashboard, and comments

--- src/app.js
+++ src/app.js
@@ -18,11 +18,26 @@ app.use(cookieParser())
 
 //routes
 import userRouter from "./routes/user.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionsRouter from "./routes/subscription.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import commentRouter from "./routes/comment.routes.js"
 
 
 //routes declaration
 app.use("/api/v1/users",userRouter)
-
+app.use("/api/v1/videos",videoRouter)
+app.use("/api/v1/tweets",tweetRouter)
+app.use("/api/v1/subscriptions",subscriptionsRouter)
+app.use("/api/v1/playlists",playlistRouter)
+app.use("/api/v1/likes",likeRouter)
+app.use("/app/v1/healthcheck",healthcheckRouter)
+app.use("/api/v1/dashboard",dashboardRouter)
+app.use("/api/v1/commentRouter",commentRouter)
 
 
 export { app }
